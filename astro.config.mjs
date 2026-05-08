accc1d996cb3f5844c9f80c239879950b31280a4
Emmanuel <28632727+OladBay@users.noreply.github.com>
2026-05-08 13:14:53 +0100

    add github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  site: "https://OladBay.github.io",
+  base: "/createdbymo",
+  devToolbar: {
+    enabled: false,
+  },
   server: {
     allowedHosts: ["overdrive-rebuilt-cobweb.ngrok-free.dev"],
+    hmr: {
+      clientPort: 443,
+      protocol: "wss",
+    },
   },
 });
